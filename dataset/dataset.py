8c2be489421b76841ad13292cbfd9c26b872f7d8
chris <s1093726@studenti.univpm.it>
2023-10-01 16:44:57 +0200

    Piccola corridge

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -12,6 +12,11 @@ if __name__ == "__main__":
     nodes = [] #Array di array di array. Il primo racchiude tutti i grafi; il sotto-array tutti i nodi di quel grafo.
     edges = [] #Stesso di sopra, ma con gli archi.
 
+    # array che contiene tutti i sottografi del grafo in esame
+    subgraph_nodes = [] #Array di coppie chiave-valore. La chiave è l'indice del grafo in esame, il valore è un array tridimensionale
+                        #dove, al livello più esterno ho tutti i sottografi del grafo, in quello più interno i nodi di quel sottografo.
+    subgraph_edges = [] #Stesso di sopra, ma con gli archi.
+
     for i in range(0, len(graphs)):
         graphs[i] = graphs[i][3:] #graphs[i] = grafo i-esimo
         graph_elems = graphs[i].split('\n') #graph_elems = array di stringhe; ogni stringa è un elemento di un grafo
@@ -19,10 +24,6 @@ if __name__ == "__main__":
         #Array contenenti rispettivamente i nodi e gli archi del grafo in esame
         graph_nodes = []
         graph_edges = []
-        
-        # array che contiene tutti i sottografi del grafo in esame
-        subgraph_nodes = [] #Array di array di array. Il primo racchiude tutti i grafi; il sotto-array tutti i nodi di quel grafo.
-        subgraph_edges = [] #Stesso di sopra, ma con gli archi.
         
         #Array contenenti rispettivamente gli indici dei nodi e dei nodi degli archi del grafo in esame
         index_graph_nodes = []
@@ -53,16 +54,20 @@ if __name__ == "__main__":
         if set(index_edge_nodes).issubset(set(index_graph_nodes)):
             nodes.append(graph_nodes)
             edges.append(graph_edges)
+
+            tmp_nodes = []
             
             #appendiamo i sottografi rimuovendo un nodo alla volta
-            for j in range(len(graph_nodes)-1,1,-1):
+            for j in range(len(graph_nodes)-1, 1, -1):
                 if j == len(graph_nodes)-1:
                     tmp = copy.deepcopy(graph_nodes)
                 else:
                     tmp = copy.deepcopy(tmp)
                 tmp.pop(j)
+
+                tmp_nodes.append(tmp)
                 
-                subgraph_nodes.append(tmp)
+            subgraph_nodes.append((i, tmp_nodes))
             
             #Creare subgraph_edges
             

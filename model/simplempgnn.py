add data info, add plot losses & accuracy, fix on duplicated softmax, grid search updated

model/simplempgnn.py
@@ -2,7 +2,7 @@ import json
 import torch
 import torch.nn.functional as F
 from torch.nn import Sequential as Seq, Linear, ReLU, Module
-from torch_geometric.nn import MessagePassing
+from torch_geometric.nn import MessagePassing, BatchNorm
 from torch_geometric.utils import sort_edge_index
 from typing import List
 
@@ -65,23 +65,32 @@ class SimpleMPGNN(Module):
 
         # Graph Convolutions
         self.conv1 = EdgeConv(self.num_features, self.graph_conv_layer_sizes[0]).to(self.device)
-        
+        self.batchnorm1 = BatchNorm(self.graph_conv_layer_sizes[0]).to(self.device)
+
         self.convs = torch.nn.ModuleList()
+        self.batchnorms = torch.nn.ModuleList()
+        
         for in_size, out_size in zip(self.graph_conv_layer_sizes, self.graph_conv_layer_sizes[1:]):
             self.convs.append(EdgeConv(in_size, out_size).to(self.device))
-        
+            self.batchnorms.append(BatchNorm(out_size).to(self.device))
+
         aggr_factory = AggrFactory()
         self.aggr_layer  = aggr_factory.factory(self.aggr_type, **self.aggr_args)
 
-        # Dense Layers
-        # Input size from the source code
-        #TODO: If len(dense_layer_sizes) == 0 this would fail
-        self.linear = torch.nn.Linear( aggr_factory.dl_in_channels_factor(self.aggr_type, **self.aggr_args) * self.graph_conv_layer_sizes[-1], 
-                                      self.dense_layer_sizes[0]).to(self.device)
+        # In forward, there is some tensor magic between these two "layers"
+        self.aggr_factor = aggr_factory.dl_in_channels_factor(self.aggr_type, **self.aggr_args)
+        
+        first_linear_size =  self.aggr_factor * self.graph_conv_layer_sizes[-1]
+        self.linear = torch.nn.Linear( first_linear_size, self.dense_layer_sizes[0]).to(self.device)
+        
+
         self.linears = torch.nn.ModuleList()
+        
         for in_size, out_size in zip(self.dense_layer_sizes, self.dense_layer_sizes[1:]):
             self.linears.append(Linear(in_size, out_size).to(self.device))
+
         self.linear_output = Linear(self.dense_layer_sizes[-1], self.num_output_features).to(self.device) # Final layer for output
+
         print(self)
 
     def reset_parameters(self):
@@ -92,37 +101,52 @@ class SimpleMPGNN(Module):
         if hasattr(self.aggr_layer, "reset_parameters"):
             self.aggr_layer.reset_parameters()
         
+        self.batchnorm1.reset_parameters()
+        for batchnorm in self.batchnorms:
+            batchnorm.reset_parameters()
+
         self.linear.reset_parameters()
         for linear in self.linears:
             linear.reset_parameters()
+
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         x = x.to(self.device)
         edge_index = edge_index.to(self.device)
         batch = batch.to(self.device)
-        #edge_index = sort_edge_index(edge_index, sort_by_row = False)
-        # Graph Convolution
-        x = F.relu(self.conv1(x, edge_index))
-        for conv in self.convs:
-            x = F.relu(conv(x, edge_index))
         
+        # Graph Convolutions
+        x = self.conv1(x, edge_index)
+        x = self.batchnorm1(x)
+        x = F.relu(x)
+
+        for conv, batchn in zip(self.convs, self.batchnorms):
+            x = conv(x, edge_index)
+            x = batchn(x)
+            x = F.relu(x)
+        
+        # Aggregation
         x = self.aggr_layer(x, batch)
+
         # Reshape the tensor to be able to pass it to the dense layers (flatten ?)
-        #x = torch.flatten(x)
-        #x = x.view(len(x), -1)
-        
+        x = x.view(len(x), -1)
+
         # Dropout
-        x = F.dropout(x, p=self.dropout_rate, training=self.training)
+        #x = F.dropout(x, p=self.dropout_rate, training=self.training)
 
         # Dense Layers
-        x = F.relu(self.linear(x))
+        x = self.linear(x)
+        x = F.relu(x)
+        
         for linear in self.linears:  
-            x = F.relu(linear(x))
-        x = F.relu(self.linear_output(x))
-        x = torch.softmax(x, dim=0) # Softmax to get probabilities
+            x = linear(x)
+            x = F.relu(x)
+
+        x = self.linear_output(x)
+        x = F.relu(x)
         return x # No activation function, as pytorch already applies softmax in cross-entropy loss
-    
+
     def __repr__(self):
         params = {
             "Graph Conv. Layer Sizes": self.graph_conv_layer_sizes,

plot.py
@@ -1,6 +1,7 @@
 import torch
 import os
 import argparse
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(prog='read & plot')
 parser.add_argument("-p","--path", type=str, default="results/exp", help='path to results folder')
@@ -21,8 +22,10 @@ def read_train_results(path):
     f1 = validation_results['f1scores']
     
     max_index = torch.argmax(torch.tensor(acc)).item()
-    print(f"Best epoch: {max_index+1}") 
+    print(f"Best epoch: {max_index+1}")
     print(f"Best val metrics => Accuracy: {acc[max_index]*100:.3f}%, Precision: {prec[max_index]*100:.3f}%, Recall: {rec[max_index]*100:.3f}%, F1score: {f1[max_index]*100:.3f}%")
+
+    return train_losses, validation_losses
 
 def read_test_results(path):
     file = os.path.join(path, "results_test.pth")
@@ -32,14 +35,36 @@ def read_test_results(path):
     prec = test_results['precision']
     rec = test_results['recall']
     f1 = test_results['f1score']
-    supp = test_results['support_per_class']
+    #supp = test_results['support_per_class']
 
     print(f"Test metrics => Accuracy: {acc*100:.3f}%, Precision: {prec*100:.3f}%, Recall: {rec*100:.3f}%, F1score: {f1*100:.3f}%")
-    print(f"Accuracy per class:\n{test_results['accuracy_per_class'].tolist()}")
-    print(f"Support per class:\n{supp}")
+    #print(f"Accuracy per class:\n{test_results['accuracy_per_class'].tolist()}")
+    
+    return test_results['accuracy_per_class'].tolist()
     
 if __name__ == "__main__":
     args = parser.parse_args()
     results_path = args.path
-    read_train_results(results_path)
-    read_test_results(results_path)
+    train_losses, validation_losses = read_train_results(results_path)
+    accuracy_per_class = read_test_results(results_path)
+    
+    plt.figure()
+    plt.plot(train_losses, label = "train")
+    plt.plot(validation_losses, label = "validation")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title("Average train and validation loss over batches")
+    plt.show()
+
+    '''plot barchart of accuracy per class with 24 classes.
+    Each bar is labeled with value of accuracy per class and the class number'''
+    plt.figure()
+    plt.bar(range(24), accuracy_per_class)
+    plt.xlabel("Class")
+    plt.ylabel("Accuracy")
+    plt.xticks(range(24))
+    plt.title("Accuracy per class in test")
+    for i, v in enumerate(accuracy_per_class):
+        plt.text(i-0.25, v + 0.025, f"{(v*100):.2f}", color='black', fontweight='bold', size = 'small')
+    plt.show()